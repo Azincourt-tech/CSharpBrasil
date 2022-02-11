88628ef5061f8be98e8d5ff83cb395212a73fb0b
Willian da Silva Almeida <williandasilvaalmeida3@gmail.com>
2022-02-11 15:17:23 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Caelum.Stella.CSharp.Validation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBrasil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string cpf1 = "86288366757";
+            string cpf2 = "98745366797";
+            string cpf3 = "22222222222";
+
+            ValidarCPF(cpf1);
+            ValidarCPF(cpf2);
+            ValidarCPF(cpf3);
+
+            Console.ReadLine();
+        }
+
+
+        private static void ValidarCPF(string cpf)
+        {
+            try
+            {
+                new CPFValidator().AssertValid(cpf);
+                Console.WriteLine("CPF VALIDO: " + cpf);
+            }
+            catch (Exception exc)
+            {
+
+                Console.WriteLine("CPF INVALIDO: " + cpf + " : " + exc.ToString());
+            }
+        }
+    }
+}
