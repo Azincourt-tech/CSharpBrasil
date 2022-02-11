e9fa3a75dba33a1e312e84da6273871ffb26c515
Willian da Silva Almeida <williandasilvaalmeida3@gmail.com>
2022-02-11 17:23:12 -0300

    Formatação de Data com e sem horario

--- TrabalhandoComData/Program.cs
+++ TrabalhandoComData/Program.cs
@@ -11,9 +11,22 @@ namespace TrabalhandoComData
     {
         static void Main(string[] args)
         {
+            //Data sem  Horario
             DateTime data = new DateTime(2017, 09, 23);
             Console.WriteLine(data.ToString("d", new CultureInfo("pt-BR")));
             Console.WriteLine(data.ToString("dd/MM"));
+
+
+            //Data com Horario
+            data = new DateTime(2017, 09, 23, 13, 14, 15, 987);
+            Console.WriteLine(data);
+            Console.WriteLine(data.ToString("HH:mm"));
+            Console.WriteLine(data.ToString("HH:mm:ss.fff"));
+
+
+
+
+
 
             Console.ReadLine();
         }
