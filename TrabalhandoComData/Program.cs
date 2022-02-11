26d3cfd08f15305f39d7490ad6ef5d78b5041658
Willian da Silva Almeida <williandasilvaalmeida3@gmail.com>
2022-02-11 16:42:02 -0300

    Criando novo projeto

---
+++ TrabalhandoComData/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhandoComData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime data = new DateTime(2017, 09, 23);
+            Console.WriteLine(data.ToString("d", new CultureInfo("pt-BR")));
+            Console.WriteLine(data.ToString("dd/MM"));
+
+            Console.ReadLine();
+        }
+    }
+}
