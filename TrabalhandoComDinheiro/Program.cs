1bda4d9002dbb83dae89ee2a3dc33b4f3e8f3e87
Willian da Silva Almeida <williandasilvaalmeida3@gmail.com>
2022-02-14 09:29:52 -0300

    Imprimindo com o padrão de moeda, BRL, EUR ou USD.

---
+++ TrabalhandoComDinheiro/Program.cs
@@ -0,0 +1,47 @@
+﻿using Caelum.Stella.CSharp.Vault;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhandoComDinheiro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Imprimindo na moeda BRL
+            Money money = 10.00;
+
+            Console.WriteLine(money.ToString());
+
+            double valor1 = 10.00;
+            double valor2 = 20.00;
+
+            Money valorTotal = valor1 + valor2;
+            Console.WriteLine(valorTotal.ToString());
+
+            decimal minuendo = 20m;
+            decimal subtraendo = 15m;
+            Money diferenca = minuendo - subtraendo;
+            Console.WriteLine(diferenca.ToString());
+
+
+            //Imprimindo na moeda EUR
+            Money euro = new Money(Currency.EUR, 1000);
+            Console.WriteLine(euro.ToString());
+
+            //Imprimindo na moeda USD
+            Money dolar = new Money(Currency.USD, 1000);
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+            Console.WriteLine(dolar.ToString());
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
